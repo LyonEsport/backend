11698157f8ef62dac31d9057216d27bb38288255
Joris Thivard <joris.thivard@soprasteria.com>
2024-05-27 09:32:51 +0200

    Model user, address, type_address, country

--- src/main/java/fr/lyonesport/esport/data/Country.java
+++ src/main/java/fr/lyonesport/esport/data/Country.java
@@ -1,7 +1,27 @@
 package fr.lyonesport.esport.data;
 
-import lombok.Data;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "country")
 public class Country {
+
+    @Id
+    @Column(name = "id_country")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "fee")
+    private Double fee;
 }
--- src/main/java/fr/lyonesport/esport/data/User.java
+++ src/main/java/fr/lyonesport/esport/data/User.java
@@ -1,7 +1,44 @@
 package fr.lyonesport.esport.data;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "app_user")
 public class User {
+
+    @Id
+    @Column(name = "id_user")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "last_name")
+    private String last_name;
+
+    @Column(name = "last_name")
+    private String first_name;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @ManyToMany
+    @JoinTable(name = "user_address",
+            joinColumns = @JoinColumn(name = "id_user"),
+            inverseJoinColumns = @JoinColumn(name = "id_address"))
+    private List<Address> addresses = new ArrayList<>();
+
 }
