cedeb4f03837e44b6f827c15f6765534b1f4d423
JALLIFIER-TALMAT Ruben <ruben.jallifier@gmail.com>
2024-05-27 11:16:32 +0200

    first test

--- src/main/java/fr/lyonesport/esport/service/UserService.java
+++ src/main/java/fr/lyonesport/esport/service/UserService.java
@@ -2,9 +2,15 @@ package fr.lyonesport.esport.service;
 
 import org.springframework.stereotype.Service;
 
+import fr.lyonesport.esport.data.User;
+
 @Service
 public class UserService {
 
     public UserService() {
+    }
+
+    public User find(String emailToTest) {
+        
     }
 }
--- src/test/java/fr/lyonesport/esport/service/UserServiceTest.java
+++ src/test/java/fr/lyonesport/esport/service/UserServiceTest.java
@@ -2,24 +2,37 @@ package fr.lyonesport.esport.service;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.security.core.userdetails.User;
 
+import fr.lyonesport.esport.data.User;
 import fr.lyonesport.esport.repository.UserRepository;
 
 class UserServiceTest {
 
     private final UserRepository userRepository = mock(UserRepository.class);
-    
-    // void setUp() {
-    //     when(userRepository.findByEmail("test@mail.com")).thenReturn(createUser());
-    // }
-    
-    @Test
-    void user_not_found_in_db_with_email() {
-        
+    private final UserService userService;
 
+    public UserServiceTest(UserService userService) {
+        this.userService = userService;
+    }
+
+    @BeforeEach
+    void setUp() {
+        when(userRepository.findByEmail("test@mail.com")).thenReturn(createUser());
+    }
+
+    @Test
+    void user_found_in_db_with_email() {
+        String emailToTest = "test@mail.com";
+        User user = userService.find(emailToTest);
+        assertNotNull(user);
+        assertEquals(emailToTest, user.getEmail());
     }
 
     @Test
@@ -27,7 +40,16 @@ class UserServiceTest {
     
     }
 
-    // User createUser() {
-    //     return new User();
-    // }
+    Optional<User> createUser() {
+        return Optional.of(
+            new User(
+                1L,
+                "test@mail.com",
+                "",
+                "",
+                "",
+                new ArrayList<>()
+            )
+        );
+    }
 }
