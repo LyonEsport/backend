37e65eb23561c4e783064668f88212c66e1811d1
Richard Cotte <rcotte1582@gmail.com>
2024-05-28 08:38:30 +0200

    correction test richard

--- src/test/java/fr/lyonesport/esport/service/OrderServiceTest.java
+++ src/test/java/fr/lyonesport/esport/service/OrderServiceTest.java
@@ -10,7 +10,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-@SpringBootTest
 class OrderServiceTest {
 
     private final OrderRepository orderRepository = mock(OrderRepository.class);
@@ -23,19 +22,18 @@ class OrderServiceTest {
     }
 
     @Test
-    void error_when_saving_on_payed_bill() throws BillMustNotBePaidException {
-        OrderService.save(createOrderPayedBill());
-        assertThrows(BillMustNotBePaidException.class, () -> OrderService.save(createOrder0Quantity()));
+    void error_when_saving_on_payed_bill() {
+        assertThrows(BillMustNotBePaidException.class, () -> OrderService.save(createOrderPayedBill()));
     }
 
     @Test
-    void error_when_saving_with_0_or_less_quantity() throws BillMustNotBePaidException {
-        OrderService.save(createOrder0Quantity());
+    void error_when_saving_with_0_or_less_quantity() {
         assertThrows(QuantityException.class, () -> OrderService.save(createOrder0Quantity()));
     }
 
     Order createOrder0Quantity() {
         Order order = new Order();
+        order.setId(createOrderId(createBill(), createUser(), createProduct()));
         order.setQuantity(0);
         order.setBill(createBill());
         order.setUser(createUser());
@@ -45,6 +43,7 @@ class OrderServiceTest {
 
     Order createOrder() {
         Order order = new Order();
+        order.setId(createOrderId(createBill(), createUser(), createProduct()));
         order.setQuantity(1);
         order.setBill(createBill());
         order.setUser(createUser());
@@ -52,8 +51,17 @@ class OrderServiceTest {
         return order;
     }
 
+    OrderId createOrderId(Bill bill, User user, Product product) {
+        OrderId orderId = new OrderId();
+        orderId.setIdUser(user.getId());
+        orderId.setIdBill(bill.getId());
+        orderId.setIdProduct(product.getId());
+        return orderId;
+    }
+
     Order createOrderPayedBill() {
         Order order = new Order();
+        order.setId(createOrderId(createBillPayedBill(), createUser(), createProduct()));
         order.setQuantity(1);
         order.setBill(createBillPayedBill());
         order.setUser(createUser());
@@ -63,23 +71,27 @@ class OrderServiceTest {
 
     Product createProduct() {
         Product product = new Product();
+        product.setId(1L);
         return product;
     }
 
     Bill createBill() {
         Bill bill = new Bill();
+        bill.setId(1L);
         bill.setIsPayed(0);
         return bill;
     }
 
     Bill createBillPayedBill() {
         Bill bill = new Bill();
+        bill.setId(2L);
         bill.setIsPayed(1);
         return bill;
     }
 
     User createUser() {
         User user = new User();
+        user.setId(1L);
         return user;
     }
 }
